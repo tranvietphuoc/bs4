4d1415fd6e279d1f7d8ccc50582334bb2331ca4d
Tran Viet Phuoc <phuoc.finn@gmail.com>
2020-05-05 20:02:14 +0700

    removed node_modules

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+function print() {
+   let a = () =>{
+        console.log("hello, world");
+    }
+}
+
+print()
+
+class Car {
+  constructor(brand) {
+    this.carName = brand;
+  }
+}
+
+myCar = new Car();
+if (a >=4) {
+
+}
